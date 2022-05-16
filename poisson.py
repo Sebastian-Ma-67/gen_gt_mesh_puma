a908e9d9988a37e89a49520589b7a91945b2fe8e
Sebastian-Ma-67 <sebastian_xq_ma@163.com>
2022-05-16 11:51:48 +0800

    update gen normal and gen gt_mesh code

---
+++ poisson.py
@@ -0,0 +1,24 @@
+import numpy as np
+import open3d as o3d
+
+from circular_buffer import buffer_to_pointcloud
+
+
+
+def run_poisson(pcd, depth, n_threads, min_density=None):
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+        pcd, depth=depth, n_threads=n_threads
+    )
+
+    # Post-process the mesh
+    if min_density:
+        vertices_to_remove = densities < np.quantile(densities, min_density)
+        mesh.remove_vertices_by_mask(vertices_to_remove)
+    mesh.compute_vertex_normals()
+
+    return mesh, densities
+
+
+def create_mesh_from_map(buffer, depth, n_threads, min_density=None):
+    pcd = buffer_to_pointcloud(buffer)
+    return run_poisson(pcd, depth, n_threads, min_density)
